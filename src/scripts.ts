ce48410111eebf4257980b7e4cbb537e03f72ca5
indcrediBro <152566488+indcrediBro@users.noreply.github.com>
2024-01-04 12:41:52 +0530

    Responsiveness and Animations with Burger Menu

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,3 +1,12 @@
-import { logDate } from "./logDate";
+import { logDate } from './logDate';
 
 logDate();
+
+function menuOnClick() {
+  document.querySelector('.menu-bar').classList.toggle('change');
+  document.querySelector('.menu').classList.toggle('change');
+  document.querySelector('.menu-bg').classList.toggle('change-bg');
+}
+
+const menu = document.querySelector('.menu-bar');
+menu.addEventListener('click', menuOnClick);
